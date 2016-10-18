af367e1152d7f43a9fef64c78068d3f676f5bc93
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-10-18 22:15:53 +0200

    Commit all changes based on https://www.youtube.com/watch?annotation_id=annotation_401609&feature=iv&src_vid=gKBiIWY7FYw&v=4DY46f-LZ0M

---
+++ hkol.tutorial.rest/src/hkol/tutorial/rest/status/V1_status.java
@@ -0,0 +1,24 @@
+package hkol.tutorial.rest.status;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/v1/status/")
+public class V1_status {
+	
+	private static final String api_version = "00.01.00";
+
+	@POST
+	@Produces(MediaType.TEXT_HTML)
+	public String returnTitle(){
+		return "<p>Java REST Web Service</p>";
+	}
+	
+	@Path("/version")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnVersion(){
+		return "<p>Java REST Web Service version: " + api_version + "<p>";
+	}
+	
+}
