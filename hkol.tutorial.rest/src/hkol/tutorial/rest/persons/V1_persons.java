cbe318da63b758f095e4cb3652af3de2802bc084
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-10-21 21:05:45 +0200

    api returnAllPersons created. ESAPI not working yet, commented out.

---
+++ hkol.tutorial.rest/src/hkol/tutorial/rest/persons/V1_persons.java
@@ -0,0 +1,52 @@
+package hkol.tutorial.rest.persons;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.codehaus.jettison.json.JSONArray;
+
+import hkol.tutorial.database.AWSMySql;
+import hkol.tutorial.util.ToJSON;
+
+@Path("/v1/persons")
+public class V1_persons {
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String returnAllPersons() throws Exception {
+		
+		PreparedStatement query = null;
+		String returnString = null;;
+		Connection conn = null;
+		
+		try {
+			conn = AWSMySql.MySqlRestConn().getConnection();
+			query = conn.prepareStatement("select * from Persons");
+			ResultSet rs = query.executeQuery();
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			query.close();
+			
+			returnString = json.toString();
+            
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+		finally {
+			if (conn != null) conn.close();
+		}
+		
+		return returnString;
+	}
+}
