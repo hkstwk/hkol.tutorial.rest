8cd8048f1622ba94f9ebad22a2dedc08f45e479d
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-12-06 23:47:04 +0100

    cleaned up some commented code in ticketcontroller.js

--- hkol.tutorial.rest/WebContent/ticketcontroller.js
+++ hkol.tutorial.rest/WebContent/ticketcontroller.js
@@ -1,10 +1,5 @@
 var app = angular.module('app',['ngResource']);
 
-//app.controller('TicketCtl', function($scope, $http) {
-//    $http.get("http://localhost:8080/hkol.tutorial.rest/api/v1/tickets")
-//    .then(function (response) {$scope.tickets = response.data;});
-//});
-
 app.controller('TicketCtl', function($scope, $http, TicketService) {
     
 	// GET all tickets
@@ -28,9 +23,4 @@ app.factory('TicketService', function ($resource) {
     return $resource('http://localhost:8080/hkol.tutorial.rest/api/v1/tickets/:ticket',{ticket: "@ticket"});
 });
 
-
-
-//app.factory('TicketService', function ($resource) {
-//    return $resource('http://jsonplaceholder.typicode.com/users/:user',{user: "@user"});
-//});
 
