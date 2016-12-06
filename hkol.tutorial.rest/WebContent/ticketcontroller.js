95e9cb8de2a7a723c9fbe4edeb67defa97407ba8
Harm Kolvoort <hkolvoort+github@gmail.com>
2016-12-06 23:05:04 +0100

    Experimenting with ng-resource. Tough!

--- hkol.tutorial.rest/WebContent/ticketcontroller.js
+++ hkol.tutorial.rest/WebContent/ticketcontroller.js
@@ -1,6 +1,36 @@
-var app = angular.module('app', []);
+var app = angular.module('app',['ngResource']);
 
-app.controller('TicketCtl', function($scope, $http) {
-    $http.get("http://localhost:8080/hkol.tutorial.rest/api/v1/tickets")
-    .then(function (response) {$scope.tickets = response.data;});
+//app.controller('TicketCtl', function($scope, $http) {
+//    $http.get("http://localhost:8080/hkol.tutorial.rest/api/v1/tickets")
+//    .then(function (response) {$scope.tickets = response.data;});
+//});
+
+app.controller('TicketCtl', function($scope, $http, TicketService) {
+    
+	// GET all tickets
+	$scope.tickets = TicketService.query();
+    
+	// GET one specific ticket
+    $scope.setDataForTicket = function(ticketID) {
+    	$scope.currentTicket = TicketService.get({ticket, ticketID});
+    }
 });
+
+app.controller('DeleteCtl', function($scope, $http) {
+    $http.delete("http://localhost:8080/hkol.tutorial.rest/api/v1/tickets/delete/" + $scope._id.$oid).
+        then(function(response) {
+        	$http.get("http://localhost:8080/hkol.tutorial.rest/api/v1/tickets")
+            .then(function (response) {$scope.tickets = response.data;});
+        });
+});
+
+app.factory('TicketService', function ($resource) {
+    return $resource('http://localhost:8080/hkol.tutorial.rest/api/v1/tickets/:ticket',{ticket: "@ticket"});
+});
+
+
+
+//app.factory('TicketService', function ($resource) {
+//    return $resource('http://jsonplaceholder.typicode.com/users/:user',{user: "@user"});
+//});
+
